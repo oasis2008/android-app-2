5f8bcd8b1c40091a352e5bf7667922581e79cd6a
SeniorZhai <developer.zhaitao@gmail.com>
2018-11-20 18:29:21 +0800

    Restore

--- app/src/main/java/one/mixin/android/ui/landing/VerificationFragment.kt
+++ app/src/main/java/one/mixin/android/ui/landing/VerificationFragment.kt
@@ -218,12 +218,14 @@ class VerificationFragment : BaseFragment() {
                 Session.storePinToken(key)
 
                 verification_keyboard.animate().translationY(300f).start()
-                mobileViewModel.insertUser(r.data!!.toUser())
                 MixinApplication.get().onlining.set(true)
                 if (account.full_name?.isBlank()!!) {
+                    mobileViewModel.insertUser(r.data!!.toUser())
                     InitializeActivity.showSetupName(context!!)
                 } else {
-                    InitializeActivity.showLoading(context!!)
+                    // InitializeActivity.showLoading(context!!)
+                    // Todo
+                     RestoreActivity.show(context!!)
                 }
                 activity?.finish()
             }, { t: Throwable ->
